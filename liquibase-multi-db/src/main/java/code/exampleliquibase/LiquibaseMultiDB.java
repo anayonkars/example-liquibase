8c8d362a444a0d0f77fecbcea04c8442bc51ea50
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2018-06-20 19:22:15 -0400

    Fixed db connectivity issues. Changed application to non-web.

--- liquibase-multi-db/src/main/java/code/exampleliquibase/LiquibaseMultiDB.java
+++ liquibase-multi-db/src/main/java/code/exampleliquibase/LiquibaseMultiDB.java
@@ -1,11 +1,15 @@
 package code.exampleliquibase;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class LiquibaseMultiDB {
     public static void main(String[] args) {
-        SpringApplication.run(LiquibaseMultiDB.class, args);
+        new SpringApplicationBuilder()
+                .web(false)
+                .sources(LiquibaseMultiDB.class)
+                .build()
+                .run(args);
     }
 }
